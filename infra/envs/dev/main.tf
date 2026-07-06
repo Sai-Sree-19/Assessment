db358f1002100cc3b9bb663bbe87ea8479ee6657
sp <sp@gmail.com>
2026-07-06 21:21:50 +0530

    Add Docker configuration

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -17,3 +17,39 @@ module "network" {
   availability_zone_2 = "ap-south-1b"
 
 }
+
+module "alb" {
+
+  source = "../../modules/alb"
+
+  vpc_id = module.network.vpc_id
+
+  public_subnet_1_id = module.network.public_subnet_1_id
+  public_subnet_2_id = module.network.public_subnet_2_id
+}
+
+module "ecs" {
+
+  source = "../../modules/ecs"
+
+  vpc_id = module.network.vpc_id
+
+  private_subnet_1_id = module.network.private_subnet_1_id
+  private_subnet_2_id = module.network.private_subnet_2_id
+
+  target_group_arn = module.alb.target_group_arn
+
+  alb_security_group_id = module.alb.alb_security_group_id
+}
+
+module "rds" {
+
+  source = "../../modules/rds"
+
+  vpc_id = module.network.vpc_id
+
+  private_subnet_1_id = module.network.private_subnet_1_id
+  private_subnet_2_id = module.network.private_subnet_2_id
+
+  ecs_security_group_id = module.ecs.ecs_security_group_id
+}
