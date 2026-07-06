3ffa5d006b58ad29bd5f7273087d1328f3564d2f
sp <sp@gmail.com>
2026-07-06 18:13:55 +0530

    Complete ALB module

---
+++ infra/Modules/alb/main.tf
@@ -0,0 +1,111 @@
+resource "aws_security_group" "alb_sg" {
+
+  name        = "alb-security-group"
+  description = "Security group for ALB"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+
+}
+
+resource "aws_lb" "alb" {
+
+  name               = "assessment-alb"
+
+  internal           = false
+
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    var.public_subnet_1_id,
+    var.public_subnet_2_id
+  ]
+
+  tags = {
+    Name = "assessment-alb"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+
+  name = "assessment-tg"
+
+  port = 80
+
+  protocol = "HTTP"
+
+  target_type = "ip"
+
+  vpc_id = var.vpc_id
+
+  health_check {
+
+    path = "/"
+
+    protocol = "HTTP"
+
+    matcher = "200"
+
+    interval = 30
+
+    timeout = 5
+
+    healthy_threshold = 2
+
+    unhealthy_threshold = 2
+
+  }
+
+  tags = {
+    Name = "assessment-target-group"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+
+  load_balancer_arn = aws_lb.alb.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  default_action {
+
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.tg.arn
+
+  }
+
+}
---
+++ infra/Modules/alb/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_1_id" {
+  description = "Public Subnet 1"
+  type        = string
+}
+
+variable "public_subnet_2_id" {
+  description = "Public Subnet 2"
+  type        = string
+}
