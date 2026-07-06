27a53e2da7bd8a797fa27c79e20d028e2790beac
sp <sp@gmail.com>
2026-07-06 17:07:07 +0530

    Complete network module

--- infra/Modules/network/main.tf
+++ infra/Modules/network/main.tf
@@ -128,3 +128,31 @@ resource "aws_route_table_association" "public_assoc_2" {
   route_table_id = aws_route_table.public_rt.id
 
 }
+
+resource "aws_route_table" "private_rt" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc_1" {
+
+  subnet_id = aws_subnet.private_subnet_1.id
+
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+
+  subnet_id = aws_subnet.private_subnet_2.id
+
+  route_table_id = aws_route_table.private_rt.id
+}
