24962fa1d6d060074c5a54999aafe74dc2e58e29
sp <sp@gmail.com>
2026-07-06 16:24:13 +0530

    Add VPC resource

---
+++ infra/Modules/network/variables.tf
@@ -0,0 +1,34 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_1_cidr" {
+  description = "Public Subnet 1 CIDR"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 CIDR"
+  type        = string
+}
+
+variable "private_subnet_1_cidr" {
+  description = "Private Subnet 1 CIDR"
+  type        = string
+}
+
+variable "private_subnet_2_cidr" {
+  description = "Private Subnet 2 CIDR"
+  type        = string
+}
+
+variable "availability_zone_1" {
+  description = "Availability Zone 1"
+  type        = string
+}
+
+variable "availability_zone_2" {
+  description = "Availability Zone 2"
+  type        = string
+}
